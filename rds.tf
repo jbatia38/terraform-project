940814749034c9ba6336624f67020bd43dad59b4
John Batia <jbatia.38@gmail.com>
2024-02-12 22:19:12 -0600

    created rds file

--- rds.tf
+++ rds.tf
@@ -1,32 +1,25 @@
-# create database subnet group
-# terraform aws db subnet group
 resource "aws_db_subnet_group" "database_subnet_group" {
-  name         = "database subnets"
-  subnet_ids   = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id]
-  description  = "subnets for database instance"
+name = "database-subnets"
+subnet_ids = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id]
+description = "subnets for database instance"
 
-  tags   = {
-    Name = "database subnets"
-  }
+tags = {
+Name = "database-subnets"
+}
 }
 
-# get the latest db snapshot
-# terraform aws data db snapshot
-data "aws_db_snapshot" "latest_db_snapshot" {
-  db_snapshot_identifier = var.database_snapshot_identifier
-  most_recent            = true
-  snapshot_type          = "manual"
-}
-
-# create database instance restored from db snapshots
-# terraform aws db instance
-resource "aws_db_instance" "database_instance" {
-  instance_class          = var.database_instance_class
-  skip_final_snapshot     = true
-  availability_zone       = "us-east-1b"
-  identifier              = var.database_instance_identifier
-  snapshot_identifier     = data.aws_db_snapshot.latest_db_snapshot.id
-  db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
-  multi_az                = var.multi_az_deployment
-  vpc_security_group_ids  = [aws_security_group.database_security_group.id]
+# create the rds instance
+resource "aws_db_instance" "db_instance" {
+engine = "mysql"
+engine_version = "8.0.31"
+multi_az = false
+identifier = "dev-rds-instance"
+username = "admin"
+password = "Gx675313*"
+instance_class = "db.t2.micro"
+allocated_storage = 200
+db_subnet_group_name = aws_db_subnet_group.database_subnet_group.name
+vpc_security_group_ids = [aws_security_group.database_security_group.id]
+availability_zone = "us-east-1b"
+db_name = "applicationdb"
 }
